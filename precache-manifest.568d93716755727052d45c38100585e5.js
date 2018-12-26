062b1b5dd0906a4ae38fa83c54a5226efbf8f37a
liupeng <1129638103@qq.com>
2018-12-26 21:08:31 +0800

    first commit

---
+++ precache-manifest.568d93716755727052d45c38100585e5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "588079df8f1b8fde3b3cd691ee65bf06",
+    "url": "./static/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "043d3c182960ea94ce6e",
+    "url": "./static/js/vendor.043d3c18.js"
+  },
+  {
+    "revision": "49953469e2f2a09edd99f1e30fc3b3f0",
+    "url": "./static/manifest.json"
+  },
+  {
+    "revision": "34ea6a22840d02f3dcf2",
+    "url": "./static/js/vue.34ea6a22.js"
+  },
+  {
+    "revision": "047f3021e153c19951f86dd209904243",
+    "url": "./static/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "0a093f3d62887bbf0037",
+    "url": "./static/js/manifest.0a093f3d.js"
+  },
+  {
+    "revision": "570dbda412175011c6e3",
+    "url": "./static/css/index.52691f08.css"
+  },
+  {
+    "revision": "b451e38c6ee4b03781b1019c518e7894",
+    "url": "./static/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "3742528143340e36915adf388d64060f",
+    "url": "./static/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "570dbda412175011c6e3",
+    "url": "./static/js/index.570dbda4.js"
+  },
+  {
+    "revision": "4dc6fdc67611efce36a3c500cbe1c0ec",
+    "url": "./static/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "0ff69c8fca81469c28f589d5fad0616e",
+    "url": "./static/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "83409fb3fe88cb94d7d3df5fe97569c9",
+    "url": "./static/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "7264d2ec7553bb96307af4acbe697faf",
+    "url": "./static/img/icons/favicon.ico"
+  },
+  {
+    "revision": "efbe0aa485b3aeaceb8ead9191f0c4c0",
+    "url": "./static/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "b8a42c4a3b00c4f75bc636f2923eaeca",
+    "url": "./static/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "418c82acd4022bfa8765bc6c9144b7ad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7264d2ec7553bb96307af4acbe697faf",
+    "url": "./favicon.ico"
+  }
+];
